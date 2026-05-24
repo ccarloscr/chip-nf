5fa075084629f5c1c6284e2ae003fcb2bd732cd1
Carlos Camilleri-Robles <134629785+ccarloscr@users.noreply.github.com>
2026-05-24 18:55:58 +0200

    Modify run_chipseq.sh for Nextflow execution
    Updated SLURM job parameters and script execution for Nextflow.

--- run_chipseq.sh
+++ run_chipseq.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=chipseq
-#SBATCH --partition=irbio01
-#SBATCH --cpus-per-task=24
-#SBATCH --time=10:00:00
-#SBATCH --output=chipseq_%j.out
-#SBATCH --error=chipseq_%j.err
 
-# Load the nextflow module
-module load nextflow
+#SBATCH --job-name=nextflow-head
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4G
+#SBATCH --time=48:00:00
+#SBATCH --output=nextflow_%j.log
 
-# Run the nextflow script
-nextflow run chipseq.nf
+export JAVA_HOME=~/java-17
+export JAVA_CMD=~/java-17/bin/java
+export PATH=~/java-17/bin:$PATH
+
+~/nextflow_versions/24.04.3/nextflow run main.nf -profile slurm -resume
